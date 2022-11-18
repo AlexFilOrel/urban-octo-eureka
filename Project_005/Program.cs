78ff799f7843b91cca256f149cd15581cb6261c0
AlexFilOrel <orel-alex@ya.ru>
2022-11-18 16:43:50 +0300

    Project_005 If-Else

---
+++ Project_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alexander")
+{
+    Console.WriteLine("Ура, это же ALEXANDERrrrr");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
